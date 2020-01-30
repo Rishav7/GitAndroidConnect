20c083032900bb3051e0040f4af19a1243abeb02
Rishav7 <rajrishav381.rr@gmail.com>
2020-01-30 21:18:32 +0530

    Main activity AND MSSG

--- app/src/main/java/com/example/gitandroidconnect/MainActivity.java
+++ app/src/main/java/com/example/gitandroidconnect/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("myTag", "messge ");
+        Log.i("myTag", "messge ");
     }
 }
